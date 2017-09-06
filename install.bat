486933a17e2acda07118fbe857796e640b9ea788
Will Rogers <wjrogers@gmail.com>
2017-09-06 09:31:22 -0400

    Use SetX after REG DELETE to trigger env refresh
    The current Windows login session doesn't re-read the environment from
    the registry unless something tells it to. SetX sends the necessary
    message, so delete first before we set.

--- install.bat
+++ install.bat
@@ -14,12 +14,12 @@ MKLINK "%USERPROFILE%\.tidyrc" "%BASE%\tidyrc"
 MKLINK "%USERPROFILE%\.vimrc" "%BASE%\vimrc"
 MKLINK /D "%USERPROFILE%\vimfiles" "%BASE%\vim"
 
-REM Configure environment
-SetX GIT_SSH "%USERPROFILE%\Dropbox\Applications\putty\plink.exe"
-SetX HTML_TIDY "%USERPROFILE%\.tidyrc"
-
 REM Delete old environment variables
 REG DELETE HKCU\Environment /V HOME /F
 REG DELETE HKCU\Environment /V GITHUB_USER /F
 REG DELETE HKCU\Environment /V GITHUB_TOKEN /F
 REG DELETE HKCU\Environment /V TERM /F
+
+REM Configure environment
+SetX GIT_SSH "%USERPROFILE%\Dropbox\Applications\putty\plink.exe"
+SetX HTML_TIDY "%USERPROFILE%\.tidyrc"
