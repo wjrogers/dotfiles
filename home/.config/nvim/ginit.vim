2abb1691caf7b23002f72fd2cde8ec44e3571c27
Will Rogers <wjrogers@gmail.com>
2019-11-25 11:18:45 -0500

    Configure neovim

---
+++ home/.config/nvim/ginit.vim
@@ -0,0 +1,5 @@
+" nvim-qt doesn't seem to support a LIST of fonts
+GuiFont Hack:h10
+
+" use nvim's tabline instead of the external one
+GuiTabline 0
--- install.ps1
+++ install.ps1
@@ -56,6 +56,7 @@ $Links = @{
     "$HOME/vimfiles" = "$Base/home/.vim"
     "$Env:APPDATA/ConEmu.xml" = "$HOME/Dropbox/Applications/ConEmu.xml"
     "$HOME/.hyper.js" = "$HOME/Dropbox/.hyper.js"
+    "$Env:LOCALAPPDATA/nvim" = "$Base/home/.config/nvim"
 }
 foreach ($Source in $Links.Keys) {
     $Target = Resolve-Path $Links[$Source]
