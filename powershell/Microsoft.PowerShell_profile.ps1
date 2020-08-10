3ffbf0fb7d255c6fb98cea02089cf593ed15af11
Will Rogers <wjrogers@gmail.com>
2020-08-10 10:51:08 -0400

    Use zoxide instead of z.lua on Windows
    I couldn't get z.lua to work on a new machine. Not sure what was wrong,
    but zoxide seems better, anyway.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,34 +1,18 @@
 Function Prompt {
-  $savedLastExitCode = $LASTEXITCODE
-
   $Location = Get-Location
   $host.UI.RawUI.WindowTitle = $Location
-
-  # handle z.lua history ourselves to preserve LASTEXITCODE
-  if ($global:_zlua_inited) {
-    _zlua --update
-  }
-
-  $LASTEXITCODE = $savedLastExitCode
   "PS $Location>"
 }
 
 # environment
 $Env:FZF_DEFAULT_COMMAND = 'fd --type file --color=always --follow --hidden --exclude .git'
 $Env:FZF_DEFAULT_OPTS = '--ansi --layout=reverse --border'
-$Env:_ZL_NO_PROMPT_COMMAND = $true
 
 # PSFzf
 Remove-PSReadlineKeyHandler 'Ctrl+r'
 Import-Module PSFzf
 
-# z.lua
-if (Test-Path "$Env:DOTFILES_HOME/z.lua") {
-  Invoke-Expression ($(lua "$Env:DOTFILES_HOME/z.lua" --init powershell) -join "`n")
-
-  # z.lua -- customize and replace the default alias
-  Remove-Alias z
-  Function z { _zlua -I $Args }
-  Function zc { _zlua -I -c $Args }
-  Function zh { _zlua -I -t . }
+# zoxide
+if (Get-Command "zoxide" -ErrorAction SilentlyContinue) {
+  Invoke-Expression ($(zoxide init --hook "pwd" powershell) -join "`n")
 }
