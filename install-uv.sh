ddebf4909eb5fbe542f2eab1310e1832bca8713f
Will Rogers <wjrogers@gmail.com>
2026-01-29 14:47:56 -0500

    Update tool versions

--- install-uv.sh
+++ install-uv.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 
 UV_PKG=$(readlink -f ~/.cache/uv.tar.gz)
-curl -fSL# -z "$UV_PKG" -o "$UV_PKG" https://github.com/astral-sh/uv/releases/download/0.9.6/uv-x86_64-unknown-linux-gnu.tar.gz
+curl -fSL# -z "$UV_PKG" -o "$UV_PKG" https://github.com/astral-sh/uv/releases/download/0.9.26/uv-x86_64-unknown-linux-gnu.tar.gz
 tar -C ~/.local/bin -xzf "$UV_PKG" --strip-components=1 
