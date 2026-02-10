f86c8452102a2d23ab9186ca40f07124faa10cb8
Will Rogers <wjrogers@gmail.com>
2026-02-10 12:23:38 -0500

    powershell: set EDITOR=nvim when present

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -8,6 +8,9 @@ Function Prompt {
 [console]::InputEncoding = [console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
 
 # environment
+if (Get-Command "nvim" -ErrorAction Ignore) {
+  $Env:EDITOR = "nvim"
+}
 $Env:FZF_DEFAULT_COMMAND = 'fd --type file --color=always --follow --hidden --exclude .git'
 $Env:FZF_DEFAULT_OPTS = '--ansi'
 $Env:FZF_CTRL_T_COMMAND = $Env:FZF_DEFAULT_COMMAND
